8454b1636c0ea204e47e88c723da2dd560e5c4ab
sky <140752918+AkashskyX@users.noreply.github.com>
2023-12-13 23:17:05 +0530

    removed image preview need to optimize look in the other branch

--- src/lib/Item/item.svelte
+++ src/lib/Item/item.svelte
@@ -48,48 +48,11 @@
   /**
    * @param {Iterable<number>} buffer
    */
-  function arrayBufferToBase64(buffer) {
-    return btoa(
-      new Uint8Array(buffer)
-        .reduce((data, byte) => data + String.fromCharCode(byte), '')
-    );
-  }
-
-  async function loadImagePreview() {
-    if (!isDir && isImageFile(name)) {
-      try {
-        const imageBytes = await invoke('thumbnail_generate', { path });
-
-       
-        previewSrc = `data:image/png;base64,${arrayBufferToBase64(imageBytes)}`;
-      } catch (error) {
-        console.error('Error loading image preview:', error);
-      }
-    }
-  }
+ 
 
 
-  onMount(() => {
-    if (!isDir) {
-        const observer = new IntersectionObserver(entries => {
-      // Logic for when the image enters the viewport
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          // Load the image only if it is in the viewport
-          loadImagePreview();
-          // Stop observing the image if it's been loaded
-          observer.unobserve(imageElement);
-        }
-      });
-    });
-
-    observer.observe(imageElement);
-
-    return () => {
-      observer.disconnect();
-    };
-    }
-  });
+  
+  
 
 
 
@@ -99,18 +62,18 @@
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <!-- svelte-ignore a11y-no-static-element-interactions -->
   <div
+  bind:this={imageElement}
     class="directory-item flex flex-col items-center w-36 m-3 hover:bg-gray-50 rounded-sm cursor-pointer"
     on:click={handleClick}
   >
     {#if isDir}
       <Icon icon="arcticons:folder" width="60" />
 
-    {:else if isImageFile(name)}
+    {:else if isImageFile(name) }
 
   
-      <!-- Image preview -->
-      <img bind:this={imageElement} src={previewSrc} alt={name} class="thumbnail" />
-      <!-- <Icon icon="arcticons:image-resizer" /> -->
+    
+      <Icon icon="arcticons:image-resizer" width="60" />
     
     {:else}
       <Icon icon="arcticons:file" width="60" />
